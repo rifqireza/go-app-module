3e28d4bb955e4eabb8653a135011b008e6391a0b
rifqireza <mahesareza53@gmail.com>
2023-02-09 12:16:22 +0700

    myModule

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rifqireza/go-app-module
+
+go 1.19
+
+require github.com/rifqireza/go-lat-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	myModule "github.com/rifqireza/go-lat-module"
+)
+
+func main() {
+	fmt.Println(myModule.SayHello())
+}
